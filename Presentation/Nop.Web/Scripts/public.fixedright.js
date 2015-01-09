11d22e76ffa6af9bdd5b06bfec9f4967c19cc30b
Henry <henry.hyh@gmail.com>
2015-01-09 11:04:24 +0800

    添加回到顶部功能

---
+++ Presentation/Nop.Web/Scripts/public.fixedright.js
@@ -0,0 +1,27 @@
+﻿$(function () {
+    var container = $("<div></div>")
+        .addClass("fixed-right")
+        .appendTo("body");
+
+    $('<div title="回到主页"><i class="fa fa-home"></i></div>')
+       .addClass("icon-home")
+       .addClass("icon")
+       .click(function () {
+           window.location.href = "/";
+       }).appendTo(container);
+
+    var scrollTop = $('<div title="回到顶部"><i class="fa fa-arrow-up"></i></div>')
+        .addClass("icon-scroll-top")
+        .addClass("icon")
+        .addClass("icon-hide")
+        .click(function () {
+            $("html, body").animate({ scrollTop: 0 }, "slow");
+        }).appendTo(container);
+
+    $(document).scroll(function () {
+        if ($(this).scrollTop() <= 100)
+            scrollTop.fadeOut("fast");
+        else
+            scrollTop.fadeIn("fast");
+    });
+});
